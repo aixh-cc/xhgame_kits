56ae08b0298df6b91f4d28a4afbd200345fd740e
hottredpen <277488199@qq.com>
2025-10-21 12:08:01 +0800

    asset

--- 3dDemo/assets/script/comps/player/PlayerLoginComp.ts
+++ 3dDemo/assets/script/comps/player/PlayerLoginComp.ts
@@ -1,4 +1,4 @@
-import { DI, System } from "@aixh-cc/xhgame_ec_framework"
+import { autoBindForDI, DI, System } from "@aixh-cc/xhgame_ec_framework"
 import { xhgame } from "../../xhgame"
 import { BaseModelComp } from "@aixh-cc/xhgame_ec_framework"
 import { SdkComp } from "../common/SdkComp"
@@ -6,13 +6,14 @@ import { PlayerModelComp } from "../models/PlayerModelComp"
 
 export class PlayerLoginSystem extends System {
     static async initComp(comp: PlayerLoginComp) {
+        const playerModelComp = DI.make<PlayerModelComp>('PlayerModelComp')
         let ret = await xhgame.gameEntity.getComponent(SdkComp).actions.login()
-        await DI.make<PlayerModelComp>('PlayerModelComp').actions.getAccount({
+        await playerModelComp.actions.getAccount({
             code: ret.code,
             anonymousCode: ret.anonymousCode,
         })
         console.log('postPlayerEnter')
-        await DI.make<PlayerModelComp>('PlayerModelComp').actions.postPlayerEnter()
+        await playerModelComp.actions.postPlayerEnter()
     }
 }
 
